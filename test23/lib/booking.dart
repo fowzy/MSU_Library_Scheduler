bdecabd914a91f563cc13f624c96244299786c97
fowzy <fwzsas@gmail.com>
2022-02-13 09:22:43 -0600

    Update

--- test23/lib/booking.dart
+++ test23/lib/booking.dart
@@ -11,8 +11,84 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:test23/confirm.dart';
-import 'package:booking_calendar/booking_calendar.dart';
+import 'package:test23/main.dart';
+import 'package:enough_mail/enough_mail.dart';
+
+Future<void> sendAnEmail(numberOfGuests, schedualingTime) async {
+  String userName = 'msubooking@gmail.com';
+  String password = 'msubooking123';
+  String mNumber = mNumberController.text;
+  String studentEmail = emailController.text;
+  // String imapServerHost = 'imap.gmail.com';
+  // int imapServerPort = 993;
+  // bool isImapServerSecure = true;
+  // String popServerHost = 'pop.gmail.com';
+  // int popServerPort = 995;
+  // bool isPopServerSecure = true;
+  String smtpServerHost = 'smtp.gmail.com';
+  int smtpServerPort = 465;
+  bool isSmtpServerSecure = true;
+  // final client = PopClient(isLogEnabled: false);
+  // try {
+  //   await client.connectToServer(popServerHost, popServerPort,
+  //       isSecure: isPopServerSecure);
+  //   await client.login(userName, password);
+  //   // alternative login:
+  //   // await client.loginWithApop(userName, password); // optional different login mechanism
+  //   final status = await client.status();
+  //   print(
+  //       'status: messages count=${status.numberOfMessages}, messages size=${status.totalSizeInBytes}');
+  //   final messageList = await client.list(status.numberOfMessages);
+  //   print(
+  //       'last message: id=${messageList?.first?.id} size=${messageList?.first?.sizeInBytes}');
+  //   var message = await client.retrieve(status.numberOfMessages);
+  //   message = await client.retrieve(status.numberOfMessages + 1);
+  //   print('trying to retrieve newer message succeeded');
+  //   await client.quit();
+  // } on PopException catch (e) {
+  //   print('POP failed with $e');
+  // }
+  final client = SmtpClient('enough.de', isLogEnabled: true);
+  try {
+    await client.connectToServer(smtpServerHost, smtpServerPort,
+        isSecure: isSmtpServerSecure);
+    await client.ehlo();
+    await client.login(userName, password);
+    final builder = MessageBuilder.prepareMultipartAlternativeMessage();
+    builder.from = [
+      MailAddress('MSU Library Scheduler', 'msubooking@gmail.com')
+    ];
+    // Dest. Email (Library Front Desk Email)
+    builder.to = [MailAddress('$mNumberController.text', 'fwzsas@gmail.com')];
+    builder.subject = '[Moffett Library] Study Room Scheduled';
+    builder.addTextHtml(
+        '<p>from: <b>MSU Library Scheduler</b></p>This email is confirmation for study room booking from Library Scheduler app.<br>Student M# $mNumber <br>Student email: $studentEmail <br>Room Capacity: $numberOfGuests guests.<br>Time Period: $schedualingTime <br>Included: <br><b>NOTE:</b>Make sure student show up at least 5 minutes ealier than the booking time and make sure you have your student your ID with you.<p><img src="https://i.ibb.co/1dp9sb4/unnamed.png"><br><b>Contact Moffett Library</b><br><b>Email: </b><a href="mailto:library@msutexas.edu" title="Email Moffett Library">library@msutexas.edu</a><br><b>Phone:</b><a href="tel:19403974204">(940) 397-4204</a><br><b>Toll-Free:</b><a href="tel:18002598518">(800) 259-8518</a>');
+    final mimeMessage = builder.buildMimeMessage();
+    final sendResponse = await client.sendMessage(mimeMessage);
+    print('message sent: ${sendResponse.isOkStatus}');
+  } on SmtpException catch (e) {
+    print('SMTP failed with $e');
+  }
+}
+
+// import 'package:booking_calendar/booking_calendar.dart';
+List<String> getAvailableTime() {
+  final now = DateTime.now();
+  final currentHour = now.hour;
+  List<String> time_list = [];
+  if (currentHour > 23) {
+    return ["No Available Time."];
+  } else {
+    int start = 8;
+    if (currentHour > 8) {
+      start = currentHour;
+    }
+    for (int i = start; i <= 23; i++) {
+      time_list.add("$i - ${i + 1}");
+    }
+    return time_list;
+  }
+}
 
 class Booking extends StatefulWidget {
   const Booking({Key? key}) : super(key: key);
@@ -22,64 +98,110 @@ class Booking extends StatefulWidget {
 }
 
 class _BookingState extends State<Booking> {
-  final now = DateTime.now();
-  late BookingService mockBookingService;
-
+  String schedualingTime = getAvailableTime()[0];
+  String numberOfGuests = "1";
   @override
-  void initState() {
-    super.initState();
-    mockBookingService = BookingService(
-        serviceName: 'Mock Service',
-        serviceDuration: 30,
-        bookingEnd: DateTime(now.year, now.month, now.day, 8, 0),
-        bookingStart: DateTime(now.year, now.month, now.day, 18, 0));
-  }
-
-  Stream<dynamic>? getBookingStreamMock(
-      {required DateTime end, required DateTime start}) {
-    return Stream.value([]);
-  }
-
-  Future<dynamic> uploadBookingMock(
-      {required BookingService newBooking}) async {
-    await Future.delayed(const Duration(seconds: 1));
-    converted.add(DateTimeRange(
-        start: newBooking.bookingStart, end: newBooking.bookingEnd));
-    print('${newBooking.toJson()} has been uploaded');
-  }
-
-  List<DateTimeRange> converted = [];
-
-  List<DateTimeRange> convertStreamResultMock({required dynamic streamResult}) {
-    ///here you can parse the streamresult and convert to [List<DateTimeRange>]
-    DateTime first = now;
-    DateTime second = now.add(Duration(minutes: 55));
-    DateTime third = now.subtract(Duration(minutes: 240));
-    DateTime fourth = now.subtract(Duration(minutes: 500));
-    converted
-        .add(DateTimeRange(start: first, end: now.add(Duration(minutes: 30))));
-    converted.add(
-        DateTimeRange(start: second, end: second.add(Duration(minutes: 23))));
-    converted.add(
-        DateTimeRange(start: third, end: third.add(Duration(minutes: 15))));
-    converted.add(
-        DateTimeRange(start: fourth, end: fourth.add(Duration(minutes: 50))));
-    return converted;
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-            backgroundColor: Color(0xff862633), title: Text("Booking Page")),
-        body: BookingCalendar(
-          bookingService: mockBookingService,
-          convertStreamResultToDateTimeRanges: convertStreamResultMock,
-          getBookingStream: getBookingStreamMock,
-          uploadBooking: uploadBookingMock,
-        ),
-      ),
+          appBar: AppBar(
+              backgroundColor: Color(0xff862633), title: Text("Booking Page")),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Number of guests:"),
+                DropdownButton<String>(
+                  value: numberOfGuests,
+                  icon: const Icon(Icons.person),
+                  elevation: 16,
+                  style: const TextStyle(color: Color(0xff862633)),
+                  underline: Container(
+                    height: 2,
+                    color: Color(0xff862633),
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      numberOfGuests = newValue!;
+                    });
+                  },
+                  items: <String>['1', '2', '3', 'More']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Text("Pick a time:"),
+                ),
+                DropdownButton<String>(
+                  value: schedualingTime,
+                  icon: const Icon(Icons.av_timer),
+                  elevation: 16,
+                  style: const TextStyle(color: Color(0xff862633)),
+                  underline: Container(
+                    height: 2,
+                    color: Color(0xff862633),
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      schedualingTime = newValue!;
+                    });
+                  },
+                  items: getAvailableTime()
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Color(0xff862633),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    child: Text(
+                      'Book',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {
+                      sendAnEmail(numberOfGuests, schedualingTime);
+                      showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Alert'),
+                          content:
+                              const Text('Your booking has been confirmed.'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, 'Ok'),
+                              child: const Text('Ok'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+          //   BookingCalendar(
+          //   bookingService: mockBookingService,
+          //   convertStreamResultToDateTimeRanges: convertStreamResultMock,
+          //   getBookingStream: getBookingStreamMock,
+          //   uploadBooking: uploadBookingMock,
+          // ),
+          ),
     );
   }
 }
--- test23/lib/main.dart
+++ test23/lib/main.dart
@@ -35,6 +35,9 @@ emailIsValid(String str) {
   return email_validation.hasMatch(str);
 }
 
+TextEditingController emailController = new TextEditingController();
+TextEditingController mNumberController = new TextEditingController();
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -43,15 +46,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MSU Study Room',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -61,16 +55,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -78,10 +62,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController emailController = new TextEditingController();
-  TextEditingController mNumberController = new TextEditingController();
-  // our logic right here and function should be before override
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -103,99 +83,104 @@ class _MyHomePageState extends State<MyHomePage> {
                   style: TextStyle(fontSize: 22),
                 ),
               ])),
-      body: SingleChildScrollView(
-        physics: ClampingScrollPhysics(),
-        child: Transform.scale(
-          scale: 0.90,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Please enter a valid student email:',
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide:
-                            BorderSide(color: Color(0xfffed322), width: 1.2)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide:
-                            BorderSide(color: Color(0xff862633), width: 1.2)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide:
-                            BorderSide(color: Color(0xff862633), width: 1.2)),
-                    hintText: 'example@my.msutexas.edu',
+      body: Column(
+        children: [
+          Text('Number of student:'),
+          SingleChildScrollView(
+            physics: ClampingScrollPhysics(),
+            child: Transform.scale(
+              scale: 0.90,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Please enter a valid student email:',
                   ),
-                ),
-              ),
-              Text('Please enter your M#:'),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-                child: TextFormField(
-                  controller: mNumberController,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide:
-                            BorderSide(color: Color(0xfffed322), width: 1.2)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide:
-                            BorderSide(color: Color(0xff862633), width: 1.2)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide:
-                            BorderSide(color: Color(0xff862633), width: 1.2)),
-                    hintText: 'M12345678',
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    child: TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide(
+                                color: Color(0xfffed322), width: 1.2)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide(
+                                color: Color(0xff862633), width: 1.2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide(
+                                color: Color(0xff862633), width: 1.2)),
+                        hintText: 'example@my.msutexas.edu',
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Color(0xff862633),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: Text(
-                    'Next',
-                    style: TextStyle(fontSize: 24),
+                  Text('Please enter your M#:'),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    child: TextFormField(
+                      controller: mNumberController,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide(
+                                color: Color(0xfffed322), width: 1.2)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide(
+                                color: Color(0xff862633), width: 1.2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide(
+                                color: Color(0xff862633), width: 1.2)),
+                        hintText: 'M12345678',
+                      ),
+                    ),
                   ),
-                  onPressed: () {
-                    if (emailIsValid(emailController.text) &&
-                        validateMnumber(mNumberController.text)) {
-                      print("is valid");
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Booking()));
-                    } else {
-                      showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: const Text('Alert'),
-                          content: const Text('Please enter valid email'),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () => Navigator.pop(context, 'Ok'),
-                              child: const Text('Ok'),
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xff862633),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 10),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20))),
+                      child: Text(
+                        'Next',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      onPressed: () {
+                        // if (emailIsValid(emailController.text) &&
+                        //     validateMnumber(mNumberController.text)) {
+                        print("is valid");
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Booking()));
+                        // } else {
+                        //   showDialog<String>(
+                        //     context: context,
+                        //     builder: (BuildContext context) => AlertDialog(
+                        //       title: const Text('Alert'),
+                        //       content: const Text('Please enter valid email'),
+                        //       actions: <Widget>[
+                        //         TextButton(
+                        //           onPressed: () => Navigator.pop(context, 'Ok'),
+                        //           child: const Text('Ok'),
+                        //         ),
+                        //       ],
+                        //     ),
+                        //   );
+                        // }
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
